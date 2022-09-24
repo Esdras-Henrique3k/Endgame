e5ec204411c57b182b380933ede5ffe289266e8d
lipe753 <112371646+lipe753@users.noreply.github.com>
2022-09-24 18:15:56 -0300

    Create Passageiro.hpp
    começando a criar o header

---
+++ Passageiro.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+class passageiro{
+
+string nome;
+int idade;
+double peso;
+int viagem;
+
+passageiro(string _nome, int _idade, double _peso);
+incrementarHistorico();
+
+
+};
