1bf3f32b547737e8ef82dcc3457c6cef9c90fce9
lipe753 <112371646+lipe753@users.noreply.github.com>
2022-09-24 18:33:59 -0300

    Update motorista.hpp

--- motorista.hpp
+++ motorista.hpp
@@ -5,11 +5,15 @@ class motorista{
   string nome;
   int idade;
   double peso;
-  int viagens();
+  int experiencia; /* numero de viagens*/
+  char estado;
+  /* pensei em dividir em 3 estados (a,b,c) sendo 'c' o pior estado e 'a' o melhor estado, para assim tornar um pré requisito 
+  a depender da previsão de duração da viagem*/
   
-  motorista(string _nome, int _idade, double _peso);
-  bool disponibilidade();
-  void estado();
-  void experiencia();
+  motorista(string _nome, int _idade, double _peso, char _estado);
+  void incrementarExperiencia();
+  void getEstado();
+  void getExperiencia();
+  void getPeso();
   
 }
