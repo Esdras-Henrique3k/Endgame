4f86ba142e71391f1939669f26e8331c63bbcad7
lipe753 <112371646+lipe753@users.noreply.github.com>
2022-09-25 10:05:19 -0300

    Update viagem.hpp

--- viagem.hpp
+++ viagem.hpp
@@ -13,6 +13,8 @@ class viagem{
   void adicionarPassageiro();
   ~viagem();
   
- 
-  
+
 };
+
+ /* o destructor seria utilizado quando a viagem fosse iniciada conforme os requisitos, 
+  o que inclui o peso máximo, ocupação máxima de assentos, motorista livre e disponibilidade do onibus */
