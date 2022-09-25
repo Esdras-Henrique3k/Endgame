d72e71d30a0b847ddcc685e95afe23bd06210261
lipe753 <112371646+lipe753@users.noreply.github.com>
2022-09-25 08:34:02 -0300

    Create cadastro.hpp

---
+++ cadastro.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+class cadastro{
+  
+  vector<cliente> clientes;
+  vector<funcionario> funcionario;
+ 
+  
+  public:
+  
+  cadastro();
+  void adicionarFuncionario();
+  void adicionarCliente();
+  
+};
