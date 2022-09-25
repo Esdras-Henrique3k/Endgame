0bc70bf84bbefc7224c6bf61060b4b46d895a8d3
lipe753 <112371646+lipe753@users.noreply.github.com>
2022-09-25 08:36:09 -0300

    Update funcionario.hpp

--- funcionario.hpp
+++ funcionario.hpp
@@ -10,6 +10,8 @@ class funcionario{
   /* pensei em dividir em 3 estados (a,b,c) sendo 'c' o pior estado e 'a' o melhor estado, para assim tornar um pré requisito 
   a depender da previsão de duração da viagem*/
   
+  public:
+  
   funcionario(string _nome, int _idade, double _peso, char _estado);
   void incrementarExperiencia();
   void getEstado();
