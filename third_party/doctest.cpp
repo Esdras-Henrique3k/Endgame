88b0ee1d00dbc22046067ae968f9c2b87c9237ce
Filipe Venturi <112371646+lipe753@users.noreply.github.com>
2022-12-07 00:35:25 +0000

    Doctest finalizado

--- third_party/doctest.cpp
+++ third_party/doctest.cpp
@@ -5,8 +5,30 @@
 
 TEST_CASE("Cadastro"){
 
-SUBCASE("Realizado"){CHECK_THROW(bboard::get_instance()->cadastrar(adm, adm, 123))  }
+SUBCASE("Realizado"){CHECK_NO_THROW(bboard::get_instance()->cadastrar(adm, adm, 123))  }
 SUBCASE("Negado"){CHECK_THROW_AS(bboard::get_instance()->cadastrar(adm, adm, 321), std::exception) }
+
+}
+
+TEST_CASE("Login"){
+
+SUBCASE("Realizado"){CHECK(bboard::get_instance()->login())  }
+SUBCASE("Negado"){CHECK(bboard::get_instance()->login()) }
+
+}
+
+TEST_CASE("Adicionar rota"){
+
+SUBCASE("Realizado"){CHECK(bboard::get_instance()->adicionarRota(rota("bh", "sp", 12, 2, 20, "executivo")))  }
+
+
+}
+
+TEST_CASE("Remover rota"){
+
+SUBCASE("Realizado"){CHECK(bboard::get_instance()->adicionarRota(rota("bh", "sp", 12, 2, 20, "executivo")))  }
+SUBCASE("Realizado"){CHECK(bboard::get_instance()->removerRota(rota("bh", "sp", 12, 2, 20, "executivo")))  }
+
 
 }
 
